6437226a90cc1333cdc836db7c319204dc885f6d
Davide Vitelaru <davide.vitelaru@taskize.com>
2026-05-05 16:16:43 +0100

    fix(terraform): add instance private IP to outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,10 @@ output "instance_public_ip" {
   value = aws_instance.app.public_ip
 }
 
+output "instance_private_ip" {
+  value = aws_instance.app.private_ip
+}
+
 output "alb_dns_name" {
   value = aws_lb.app.dns_name
 }
