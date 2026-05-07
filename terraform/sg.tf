e0cb2c1eb6d3531a2efdfd54637066fd6a171712
Davide Vitelaru <davide.vitelaru@taskize.com>
2026-05-07 10:36:08 +0100

    fix(terraform): broaden SSH ingress to 10.0.0.0/8

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "instance" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.250.0.0/16"]
+    cidr_blocks = ["10.0.0.0/8"]
   }
 
   ingress {
